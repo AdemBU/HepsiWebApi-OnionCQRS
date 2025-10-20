ea0a796a0d08f9b46acc3d98eb1be2979e99ebe1
AdemBU <61334304+AdemBU@users.noreply.github.com>
2025-10-20 18:09:08 +0300

    AutoMapper geliştirildi

--- Presentation/HepsiWebApi.Api/Program.cs
+++ Presentation/HepsiWebApi.Api/Program.cs
@@ -1,4 +1,6 @@
 using HepsiWebApi.Persistence;
+using HepsiWebApi.Application;
+using HepsiWebApi.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,8 @@ builder.Configuration
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddCustomMapper();
 
 var app = builder.Build();
 
